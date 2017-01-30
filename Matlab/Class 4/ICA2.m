e0a00420ecfd64a88832d5237a6a3525eefdca26
Vikas <potlurivikas@gmail.com>
2017-01-30 13:58:04 -0600

    Class 4 - ICA 2

--- Matlab/Class 4/ICA2.m
+++ Matlab/Class 4/ICA2.m
@@ -1,6 +1,32 @@
+hold on;
+grid on;
 load('steel.txt');
+strain = steel(:,2);
+stress = steel(:,4);
+time = steel(:,3);
+position = steel(:,1);
+
 subplot(2,2,1);
+plot(strain*100,stress,'b');
+xlabel('Strain (%)');
+ylabel('Stress (mPa)');
+axis([0,30,0,800]);
+grid on;
 
 subplot(2,2,2);
+plot(time,position,'r');
+xlabel('time (sec)');
+ylabel('displacement distance (mm)');
+grid on;
+
 subplot(2,2,3);
+plot(position,strain*100,'g');
+xlabel('displacement distance (mm)');
+ylabel('Strain (%)');
+grid on;
+
 subplot(2,2,4);
+plot(time,stress,'m');
+xlabel('time (sec)');
+ylabel('Stress (mPa)');
+grid on;
