e5eb0b4144992506ed4ad34a24a294d0c0f70bdd
Vikas <potlurivikas@gmail.com>
2017-01-30 13:23:12 -0600

    Dotted lines

--- Matlab/Class 4/ICA1.m
+++ Matlab/Class 4/ICA1.m
@@ -1,9 +1,9 @@
 hold on;
 load('fitdata.mat');
-scatter(x,y,'black');
+scatter(x,y,'ko');
 plot(x,yfit,'LineWidth',2,'Color','black');
-plot(x,yfit + 0.3,'LineWidth',2,'Color','red');
-plot(x,yfit - 0.3,'LineWidth',2,'Color','red');
+plot(x,yfit + 0.3,':r','LineWidth',2);
+plot(x,yfit - 0.3,':r','LineWidth',2);
 xlabel('x data');
 ylabel('y data and fit');
 legend('Data','Fit','Lower/Upper Bounds','Location','Northwest');
